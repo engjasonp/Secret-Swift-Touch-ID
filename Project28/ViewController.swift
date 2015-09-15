e2f3633b3c4ba75fe3d8926c3f741ad47341d3f4
engjasonp <engjasonp@gmail.com>
2015-09-14 23:54:41 -0700

    Added observers for keyboard frame changes. Added func adjustForKeyboard. Attached IBAction to authenticate button. IB: Moved text view in front of label, and made it hidden.

--- Project28/ViewController.swift
+++ Project28/ViewController.swift
@@ -11,8 +11,33 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var secret: UITextView!
+    
+    @IBAction func authenticateUser(sender: AnyObject) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.addObserver(self, selector: "adjustForKeyboard:", name: UIKeyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: "adjustForKeyboard:", name: UIKeyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    func adjustForKeyboard(notification: NSNotification) {
+        let userInfo = notification.userInfo!
+        
+        let keyboardScreenEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+        let keyboardViewEndFrame = view.convertRect(keyboardScreenEndFrame, fromView: view.window)
+        
+        if notification.name == UIKeyboardWillHideNotification {
+            secret.contentInset = UIEdgeInsetsZero
+        } else {
+            secret.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0)
+        }
+        
+        secret.scrollIndicatorInsets = secret.contentInset
+        
+        let selectedRange = secret.selectedRange
+        secret.scrollRangeToVisible(selectedRange)
     }
 
     override func didReceiveMemoryWarning() {
