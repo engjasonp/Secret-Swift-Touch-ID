9b3d6d80c1e9ec6be60eb6e4bc5be32f0847f023
engjasonp <engjasonp@gmail.com>
2015-09-16 02:10:48 -0700

    Imported LocalAuthentication. Added code for authenticateUser to allow device authentication with biometrics and to account for possible errors. Added code "if true {" to take place of commented out code to test app in simulator. Imported KeychainWrapper.swift to project.  IB: Fixed text view height to fit right under nav bar.

--- Project28/ViewController.swift
+++ Project28/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright (c) 2015 EngJason. All rights reserved.
 //
 
+import LocalAuthentication
 import UIKit
 
 class ViewController: UIViewController {
@@ -13,13 +14,51 @@ class ViewController: UIViewController {
     @IBOutlet weak var secret: UITextView!
     
     @IBAction func authenticateUser(sender: AnyObject) {
+        var context = LAContext()
+        var error: NSError?
         
+        if true {
+//            context.canEvaluatePolicy(.DeviceOwnerAuthenticationWithBiometrics, error: &error) {
+            var reason = "Identify yourself!"
+            
+            context.evaluatePolicy(.DeviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {
+                [unowned self] (success: Bool, authenticationError: NSError?) -> Void in
+                
+                dispatch_async(dispatch_get_main_queue()) {
+                    if true {
+//                    if success {
+                        self.unlockSecretMessage()
+                    } else {
+                        if let error = authenticationError {
+                            if error.code == LAError.UserFallback.rawValue {
+                                let ac = UIAlertController(title: "Passcode? Ha!", message: "It's Touch ID or nothing – sorry!", preferredStyle: .Alert)
+                                ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+                                self.presentViewController(ac, animated: true, completion: nil)
+                                return
+                            }
+                        }
+                        
+                        let ac = UIAlertController(title: "Authentication failed", message: "Your fingerprint could not be verified; please try again.", preferredStyle: .Alert)
+                        ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+                        self.presentViewController(ac, animated: true, completion: nil)
+                    }
+                }
+            }
+        } else {
+            let ac = UIAlertController(title: "Touch ID not available", message: "Your device is not configured for Touch ID.", preferredStyle: .Alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            self.presentViewController(ac, animated: true, completion: nil)
+        }
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let notificationCenter = NSNotificationCenter.defaultCenter()
         notificationCenter.addObserver(self, selector: "adjustForKeyboard:", name: UIKeyboardWillHideNotification, object: nil)
         notificationCenter.addObserver(self, selector: "adjustForKeyboard:", name: UIKeyboardWillChangeFrameNotification, object: nil)
+        notificationCenter.addObserver(self, selector: "saveSecretMessage", name: UIApplicationWillResignActiveNotification, object: nil)
+        title = "Nothing to see here"
+        
     }
     
     func adjustForKeyboard(notification: NSNotification) {
@@ -38,6 +77,24 @@ class ViewController: UIViewController {
         
         let selectedRange = secret.selectedRange
         secret.scrollRangeToVisible(selectedRange)
+    }
+    
+    func unlockSecretMessage() {
+        secret.hidden = false
+        title = "Secret stuff!"
+        
+        if let text = KeychainWrapper.stringForKey("SecretMessage") {
+            secret.text = text
+        }
+    }
+    
+    func saveSecretMessage() {
+        if !secret.hidden {
+            KeychainWrapper.setString(secret.text, forKey: "SecretMessage")
+            secret.resignFirstResponder()
+            secret.hidden = true
+            title = "Nothing to see here"
+        }
     }
 
     override func didReceiveMemoryWarning() {
